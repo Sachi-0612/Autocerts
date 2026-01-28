4005794c007c2169bd397a8d16a41ceb6c3ee6ab
Sachi-0612 <sachikaursidhu006@gmail.com>
2026-01-28 11:39:29 +0530

    Routing sorted

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,47 @@
 import Navbar from './components/Navbar'
-import HeroSection from './components/HeroSection'
 import React from 'react'
 import Upload from './components/heroSection/Upload'
 import DesignTemplate from './components/heroSection/DesignTemplate'
 import EmailConfig from './components/heroSection/EmailConfig'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './router/layout'
+
 // import PreviewAndConfig from './components/heroSection/PreviewAndConfig'
 
 function App() {
-
-
-  return (
-    <div className='bg-gray-200 min-h-screen'>
-      <div className='font-roboto text-4xl pt-6 px-7 font-bold'>AutoCerts</div>
-      <div className='text-md text-gray-600 py-2.5 px-7'>Upload Excel data, customize templates, and send personalized certificates or ID cards via email</div>
-      <Navbar/>
-      {/* <HeroSection/> */}
-
-      {/* <div className="flex flex-col md:flex-row justify-center gap-4 px-7">
+  const router = createBrowserRouter([
+    {
+      path: "",
+      element: <Layout/>,
+      children: [
+        { path: "/uploaddata", element: <><div className="flex flex-col md:flex-row justify-center gap-4 px-7">
       <Upload/>
       <DesignTemplate/>
-      </div> */}
-
-      <div className="flex flex-col md:flex-row justify-center gap-4 px-7">
-       <EmailConfig/>
-       {/* <PreviewAndConfig/> */}
-       </div>
+      </div></> },
+        // { path: "design", element: <DesignTemplate/> },
+        { path: "/previewsend", element: <><div className="flex flex-col md:flex-row justify-center gap-4 px-7">
+        <EmailConfig/> 
+        </div></> },
+        {
+          path: "",
+        element: <><div className="flex flex-col md:flex-row justify-center gap-4 px-7">
+      <Upload/>
+      <DesignTemplate/>
+      </div></>
+        }
+        
+      ]
+    }
+    
+  ])
+    
+  return (
+    <div className='bg-gray-200 min-h-screen'>
+       <div className='font-roboto text-4xl pt-6 px-7 font-bold'>AutoCerts</div>
+       <div className='text-md text-gray-600 py-2.5 px-7'>Upload Excel data, customize templates, and send personalized certificates or ID cards via email</div>
+      
+      {/* <Navbar/> */}
+      <RouterProvider router={router}/>
     </div>
   )
 }
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,15 @@
+// import EmailConfig from "./heroSection/EmailConfig";
+import Upload from "./heroSection/Upload";
+import DesignTemplate from "./heroSection/DesignTemplate";
+// import React, { useState } from "react";
+// import Navbar from "./Navbar";
+
 export default function HeroSection() {
-    
+    // const [value, setValue] = useState(0);
     return(
-        <div>Hero Section</div> 
+        <div className="flex justify-around items-center" >
+            <Upload/>
+            <DesignTemplate/>
+        </div>
     )
 }
